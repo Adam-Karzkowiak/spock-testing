087a6b915756067b2cb746fdc33e75c46ec3d617
Adam-Karzkowiak <adam.karzkowiak@gmail.com>
2022-01-09 19:08:02 +0100

    simple assertion test

---
+++ src/test/groovy/com/demo/spock/ExampleSpecification.groovy
@@ -0,0 +1,10 @@
+package com.demo.spock
+
+import spock.lang.Specification
+
+class ExampleSpecification extends Specification {
+    def "should be a simple assertion"() {
+        expect:
+        1 == 1
+    }
+}
